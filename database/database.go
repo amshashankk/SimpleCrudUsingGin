bb5af762d6ebfb246dd03fd187f6fc16310898e6
SHASHANK KESARWANI <52634856+amshashankk@users.noreply.github.com>
2021-12-07 11:37:59 +0530

    Updated Database

--- database/database.go
+++ database/database.go
@@ -1,21 +1,37 @@
 package database
 
 import (
-	"database/sql"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/amshashankk/models"
 
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-
-	_ "github.com/lib/pq"
 )
 
-var DB *sql.DB
-var ConnectDB *gorm.DB
-var err error
+// DB gorm connector
+var DB *gorm.DB
 
-func Connect() {
-	ConnectDB, err = gorm.Open("postgres", "user=postgress dbname=postgress password=postgress sslmode=disable")
+// connection with database
+func ConnectDB() {
+	var err error
+	p := os.Getenv("DB_PORT")
+	port, err := strconv.ParseUint(p, 10, 32)
+
+	DB, err = gorm.Open("postgres", fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		port, os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME")))
+
 	if err != nil {
-		panic(err)
+		panic("failed to connect database")
 	}
+
+	fmt.Println("Connection Opened to Database")
+
+	DB.AutoMigrate(&models.Employee{})
+	fmt.Println("Database Migrated")
 }
