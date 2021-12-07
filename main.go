0224df6bbdbc1850ffa127c46b3f15fe4516ebed
SHASHANK KESARWANI <52634856+amshashankk@users.noreply.github.com>
2021-12-07 11:37:01 +0530

    Updated main

--- main.go
+++ main.go
@@ -4,25 +4,27 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 
 	"github.com/amshashankk/database"
 	"github.com/amshashankk/routes"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 func main() {
+	app := gin.Default()
+
+	godotenv.Load()
 
 	//InitPostgres()
-	database.Connect()
+	database.ConnectDB()
 
 	fmt.Println("Server started at port 4000")
-	defer database.DB.Close()
 
-	// Set the router as the default one shipped with Gin
-	gin.SetMode(gin.ReleaseMode)
-	routes.SetupRoutes()
+	
+	routes.SetupRoutes(app)
 
-	app := gin.Default()
 	// Start and run the server
-	app.Run(":4000")
+	app.Run(":5000")
 
 }
