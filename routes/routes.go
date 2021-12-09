94a08956e837464768ac6f4961d376180c9d6473
SHASHANK KESARWANI <52634856+amshashankk@users.noreply.github.com>
2021-12-09 15:30:10 +0530

    Updated Routes

--- routes/routes.go
+++ routes/routes.go
@@ -9,8 +9,8 @@ func SetupRoutes(router *gin.Engine) {
 	api := router.Group("/api")
 
 	api.POST("/add", controllers.AddEmployee)
-	api.DELETE("/delete", controllers.DeleteEmployee)
-	api.POST("/update", controllers.UpdateEmpDetails)
+	api.DELETE("/delete/:id", controllers.DeleteEmployee)
+	api.POST("/update/:id", controllers.UpdateEmpDetails)
 	api.GET("/show/:id", controllers.GetEmployeeById)
 	api.GET("/showall", controllers.GetEmployees)
 }
